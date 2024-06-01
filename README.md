24f0522350742fb5e287fec1eb8653fd8ede86d3
Yokohama <artuzcapati2@gmail.com>
2024-06-02 00:11:00 +0800

    updated README.md, bad_apple.py and requirements.txt

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Bad Apple!!
-"[Bad Apple!!](https://youtu.be/watch?v=FtutLA63Cp8)" rendered on the terminal using Python. Yes, [two exclamation marks](https://www.reddit.com/r/japan/comments/6u5evk/how_come_double_exclamation_marks_are_so_common). TBD.
+"[Bad Apple!!](https://youtu.be/watch?v=FtutLA63Cp8)" rendered on the terminal using Python. Yes, [two exclamation marks](https://www.reddit.com/r/japan/comments/6u5evk/how_come_double_exclamation_marks_are_so_common). Runs on Python 3.11.5, 64-bit.  
+Technically, this can render any YouTube video, just don't expect it to work without (minor) issues.
+
+# How to install
+1. Download this code, put it wherever you want.
+2. Create a [virtual environment](https://realpython.com/python-virtual-environments-a-primer).
+3. Activate the environment, and type `pip install -r requirements.txt`.
+4. Install [yt-dlp](https://youtu.be/watch?v=5aYwU4nj5QA). Make sure to put it on PATH.
+4. Install [FFMPEG](https://youtu.be/watch?v=jZLqNocSQDM). Also put it on PATH.
+5. Run the program by typing `py bad_apple.py` or `python bad_apple.py` in the terminal.
+6. Wait for it to finish downloading assets, and Touhou magic will now render on the terminal.
+7. Press `CTRL + C` to stop the program from running.
+8. If something goes wrong, you can delete the `cache` folder, and it will start downloading the assets again.
+
+# Issues
+- [ ] Audio out of sync?
+- [ ] Excessive flickering of text in the terminal.
 
 > [!TIP]
 > "All weeaboo programmers must write a Bad Apple!! program at least once in their lives." - 宮崎駿
--- bad_apple.py
+++ bad_apple.py
@@ -1,40 +1,23 @@
-# write python code that plays bad apple video from youtube in the terminal using ascii characters
-# write python code that plays bad apple video from youtube in the terminal using ascii characters, use yt_dlp and ffmpeg-python
-
-# import ffmpeg
+import ctypes
+import pygame
 import os
 import shutil
 import subprocess
-import sys
+import time
 import yt_dlp
-
 from PIL import Image
-from time import sleep
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-ASCII_CHARS = [*"@#S%?*+;:,."]
-ASCII_CHARS.reverse()
-SCALE_SIZE = 70
+# ASCII_CHARS = [*" .:;+*?%S#@"]
+ASCII_CHARS = [*" .:-=+*?#%@"]
+SCALE_SIZE = 90
+FPS = 15.5
 
-def reduce_framerate_mp4(input, output):
+def extract_frames_from_mp4(input_mp4):
     ffmpeg_cmd = [
         "ffmpeg",
-        "-i", input,
-        "-filter:v", "fps=15",
-        output
-    ]
-
-    try:
-        subprocess.run(ffmpeg_cmd, check=True)
-        print("Framerate conversion successful!")
-    except subprocess.CalledProcessError as e:
-        print("Framerate conversion failed!")
-
-def extract_frames_from_mp4(input):
-    ffmpeg_cmd = [
-        "ffmpeg",
-        "-i", input,
-        "frames/frame%05d.png"
+        "-i", input_mp4,
+        "cache/frames/frame%04d.png"
     ]
 
     try:
@@ -42,17 +25,50 @@ def extract_frames_from_mp4(input):
         print("Frame extraction successful!")
     except subprocess.CalledProcessError as e:
         print("Frame extraction failed!")
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def reduce_framerate_mp4(input_mp4, output_mp4):
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-i", input_mp4,
+        "-filter:v", "fps=15",
+        output_mp4
+    ]
+
+    try:
+        subprocess.run(ffmpeg_cmd, check=True)
+        print("Framerate conversion successful!")
+    except subprocess.CalledProcessError as e:
+        print("Framerate conversion failed!")
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def extract_audio_from_mp4(input_mp4, output_mp3):
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-i", input_mp4,
+        output_mp3
+    ]
+
+    try:
+        subprocess.run(ffmpeg_cmd, check=True)
+        print("Audio extraction successful!")
+    except subprocess.CalledProcessError as e:
+        print("Audio extraction failed!")
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 # https://youtu.be/v_raWlX7tZY
 def resize_image(image, new_width=SCALE_SIZE):
     width, height = image.size
-    ratio = height/width
+    ratio = height / width
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
     return (resized_image)
 
 
-def grayify(image):
+def grayscale(image):
     return image.convert("L")
 
 
@@ -63,86 +79,106 @@ def pixels_to_ascii(image):
 
 
 def resize_images_in_frames():
-    FRAMES_FOLDER = os.listdir(f"{THIS_FOLDER}/frames")
+    FRAMES_FOLDER = os.listdir(f"{THIS_FOLDER}/cache/frames")
+    # print("")
     for item in FRAMES_FOLDER:
-        if os.path.isfile(f"{THIS_FOLDER}/frames/{item}"):
-            im = Image.open(f"{THIS_FOLDER}/frames/{item}")
-            f, _ = os.path.splitext(f"{THIS_FOLDER}/frames/{item}")
-            im_resize = im.resize((240, 180))
+        if os.path.isfile(f"{THIS_FOLDER}/cache/frames/{item}"):
+            im = Image.open(f"{THIS_FOLDER}/cache/frames/{item}")
+            f, _ = os.path.splitext(f"{THIS_FOLDER}/cache/frames/{item}")
+            im_resize = im.resize((300, 168))
             im_resize.save(f + "_resized.png", "PNG", quality=90)
-            os.remove(f"{THIS_FOLDER}/frames/{item}")
-            print(f"({item}) image resize successful.")
+            os.remove(f"{THIS_FOLDER}/cache/frames/{item}")
+            print(f"\033[1A\033[KResizing images... ({FRAMES_FOLDER.index(item)}/{len(FRAMES_FOLDER)})")
+
+
+def play_music_file(mp3_file):
+    pygame.mixer.init()
+    pygame.mixer.music.load(mp3_file)
+    pygame.mixer.music.play()
 
 
 def render_frames(new_width):
-    FRAMES_FOLDER = os.listdir(f"{THIS_FOLDER}/frames")
-    # print(FRAMES_FOLDER[1159])
-    # print(f"{FRAMES_FOLDER}\n{os.path.exists(FRAMES_FOLDER)}")
+    FRAMES_FOLDER = os.listdir(f"{THIS_FOLDER}/cache/frames")
+
+    # force maximize terminal window
+    user32 = ctypes.WinDLL("user32")
+    SW_MAXIMISE = 3
+    hWnd = user32.GetForegroundWindow()
+    user32.ShowWindow(hWnd, SW_MAXIMISE)
+
+    play_music_file("cache/audio/track.mp3")
 
     for item in FRAMES_FOLDER:
         try:
-            image = Image.open(f"frames/{item}") # RANDOM FRAME
+            image = Image.open(f"cache/frames/{item}")
         except:
             print("not a valid image")
         
-        new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+        new_image_data = pixels_to_ascii(grayscale(resize_image(image)))
         
         pixel_count = len(new_image_data)  
         ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, pixel_count, new_width)])
 
-        os.system("cls" if os.name == "nt" else "clear")
+        print(chr(27) + "[2J")
         print(ascii_image)
-        sleep(0.025)
+        time.sleep(1 / FPS) # remove the delay for debugging
+    os.system("cls" if os.name == "nt" else "clear")
+    print("\n" * 50 + "終")
 
-videos = "videos"
+
+videos = "cache/videos"
 if not os.path.exists(videos):
-    print("don't render in terminal yet")
     os.makedirs(videos)
 
-frames = "frames"
+audio = "cache/audio"
+if not os.path.exists(audio):
+    os.makedirs(audio)
+
+frames = "cache/frames"
 if not os.path.exists(frames):
     os.makedirs(frames)
+
+    # REMOVE DEBUG
     try:
-        os.remove("videos/bad_apple_temp.mp4")
+        os.remove("cache/videos/bad_apple_temp.mp4")
     except:
-        print("videos/bad_apple_temp.mp4 doesnt exist")
+        print("cache/videos/bad_apple_temp.mp4 doesnt exist")
 
     try:
-        os.remove("videos/bad_apple.mp4")
+        os.remove("cache/videos/bad_apple.mp4")
     except:
-        print("videos/bad_apple.mp4 doesnt exist")
+        print("cache/videos/bad_apple.mp4 doesnt exist")
     # UNTIL HERE
 
     ydl_opts = {
         "format": "worst",
         # "listformats": True,
         "ignoreerrors": True,
-        "outtmpl": "videos/bad_apple_temp.mp4"
+        "outtmpl": "cache/videos/bad_apple_temp.mp4"
     }
 
-    # https://youtu.be/watch?v=WJq4jWSQNd8 - Go ! Bwaaah ! (short video for debugging)
-    # https://youtu.be/watch?v=FtutLA63Cp8 - 【東方】Bad Apple!! ＰＶ【影絵】(not the official upload, but has more views than the official one)
-    # https://youtu.be/watch?v=i41KoE0iMYU - [Alstroemeria Records]Bad Apple!! feat.nomico(Shadow Animation Version)[ACVS.008_Tr.00] (official upload)
+    ids = [
+        "FtutLA63Cp8", # 【東方】Bad Apple!! ＰＶ【影絵】(not the official upload, but has more views than the official one)
+        "i41KoE0iMYU", # [Alstroemeria Records]Bad Apple!! feat.nomico(Shadow Animation Version)[ACVS.008_Tr.00] (official upload)
+        "9lNZ_Rnr7Jc", # Bad Apple!! - Full Version w/video [Lyrics in Romaji, Translation in English]
+        "v-fc1zv31zE", # wtf?
+        "Uds7g3M-4lQ", # BAND-MAID / Thrill (スリル) (Official Music Video) (AUDIO SYNC ISSUES)
+        "WJq4jWSQNd8", # - Go ! Bwaaah ! (short video for debugging)
+    ]
 
     with yt_dlp.YoutubeDL(ydl_opts) as y:
-        y.download(["https://youtu.be/watch?v=FtutLA63Cp8"])
+        y.download([f"https://youtu.be/watch?v={ids[0]}"])
 
-    reduce_framerate_mp4("videos/bad_apple_temp.mp4", "videos/bad_apple.mp4")
-    extract_frames_from_mp4("videos/bad_apple.mp4")
+    reduce_framerate_mp4("cache/videos/bad_apple_temp.mp4", "cache/videos/bad_apple.mp4")
+    extract_audio_from_mp4("cache/videos/bad_apple_temp.mp4", "cache/audio/track.mp3")
+    extract_frames_from_mp4("cache/videos/bad_apple.mp4")
     resize_images_in_frames()
-    print("then render it")
-    render_frames(SCALE_SIZE*2)
+    render_frames(SCALE_SIZE * 2)
 
     try:
-        shutil.rmtree("videos")
+        shutil.rmtree("cache/videos")
     except:
-        print("videos not deleted")
+        print("cache/videos not deleted")
 
 else:
-    print("render in terminal")
-    render_frames(SCALE_SIZE*2)
-
-""" try:
-    shutil.rmtree("frames")
-except:
-    print("frames not deleted") """
+    render_frames(SCALE_SIZE * 2)
